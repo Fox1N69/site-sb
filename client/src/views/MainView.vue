35a6895bc09ea1daffe06d28ecd412b95694da74
Fox1N69 <63461675+Fox1N69@users.noreply.github.com>
2023-06-01 06:35:19 +0500

    main page

--- client/src/views/MainView.vue
+++ client/src/views/MainView.vue
@@ -3,12 +3,15 @@
     <section class="home">
       <Navigation/>
       <div class="title">
-        <h1 class="title__h1">Сибирский рубежъ</h1>
+        <h1 class="title__h1">Сибирский  рубежъ</h1>
       </div>
       
       <div class="content">
           <div class="content__name">Ситниковский отдел Военно Исторического Клуба "Сибирский рубеж"</div>
           <div class="content__description">Военно Исторический Клуб "Сибирский рубеж" занимается углубленным изучением истории Великой Отечественной Войны</div>
+          <div class="content__solider">
+            <img class="solider" src="../assets/img/solider.png" alt="">
+          </div>
           <div class="content__video"></div>
       </div>
     </section>
