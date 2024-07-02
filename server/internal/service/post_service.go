93deeab8cade7c238f059267fb7655631e7badf0
Pavel Maksimovich <63461675+Fox1N69@users.noreply.github.com>
2024-07-02 22:32:47 +0500

    реализовал репозиторий

--- server/internal/service/post_service.go
+++ server/internal/service/post_service.go
@@ -1,1 +1,37 @@
 package service
+
+import (
+	"site-sb/internal/models"
+	"site-sb/internal/repo"
+)
+
+type PostService interface {
+	CreatePost(post *models.Post) error
+	GetPosts() ([]models.Post, error)
+	UpdatePost(postID uint) error
+	DeletePost(postID uint) error
+}
+
+type postService struct {
+	repo repo.PostRepo
+}
+
+func NewPostService(repository repo.PostRepo) PostService {
+	return &postService{repo: repository}
+}
+
+func (s *postService) CreatePost(post *models.Post) error {
+	return s.repo.Create(post)
+}
+
+func (s *postService) GetPosts() ([]models.Post, error) {
+	return s.repo.GetPosts()
+}
+
+func (s *postService) UpdatePost(postID uint) error {
+	return s.repo.Update(postID)
+}
+
+func (s *postService) DeletePost(postID uint) error {
+	return s.repo.Delete(postID)
+}
