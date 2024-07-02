dfbc40a23664c5586e49c7f6686955f0c8d3617d
Pavel Maksimovich <63461675+Fox1N69@users.noreply.github.com>
2024-07-02 23:04:54 +0500

    Implemente all methods in the post handler

--- server/internal/api/v1/post_handler.go
+++ server/internal/api/v1/post_handler.go
@@ -4,12 +4,16 @@ import (
 	"site-sb/common/http/response"
 	"site-sb/internal/models"
 	"site-sb/internal/service"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 type PostHandler interface {
 	Create(c *gin.Context)
+	GetAll(c *gin.Context)
+	Update(c *gin.Context)
+	Delete(c *gin.Context)
 }
 
 type postHandler struct {
@@ -36,4 +40,44 @@ func (h *postHandler) Create(c *gin.Context) {
 		"message": "post create success",
 		"data":    post,
 	})
+}
+
+func (h *postHandler) GetAll(c *gin.Context) {
+	post, err := h.service.GetPosts()
+	if err != nil {
+		response.New(c).Error(501, err)
+		return
+	}
+
+	c.JSON(200, post)
+}
+
+func (h *postHandler) Update(c *gin.Context) {
+	postID, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		response.New(c).Error(400, err)
+		return
+	}
+
+	if err := h.service.UpdatePost(uint(postID)); err != nil {
+		response.New(c).Error(501, err)
+		return
+	}
+
+	c.JSON(200, "post update seccess")
+}
+
+func (h *postHandler) Delete(c *gin.Context) {
+	postID, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		response.New(c).Error(400, err)
+		return
+	}
+
+	if err := h.service.DeletePost(uint(postID)); err != nil {
+		response.New(c).Error(501, err)
+		return
+	}
+
+	c.JSON(200, "post delete success")
 }
