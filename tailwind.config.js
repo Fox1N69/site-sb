14d44cbf4c1e737f8754ca83fbdb040f232fd70e
Pavel Maksimovich <63461675+Fox1N69@users.noreply.github.com>
2024-07-03 02:36:55 +0500

    Что то я поделал и устал, нихера не понял и ничего не работает

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
+// tailwind.config.cjs
+const { nextui } = require("@nextui-org/react");
+
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [],
+module.exports = {
+  content: [
+    // ...
+    // make sure it's pointing to the ROOT node_module
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
-}
-
+  darkMode: "class",
+  plugins: [nextui()],
+};
