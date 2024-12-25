54257cd9f3ce8df9698228b5c3fb934607483837
Pavel Maksimovich <diogoc1707@gmail.com>
2024-12-25 19:18:46 +0500

    change

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # Перейдите в директорию вашего приложения
-cd /sitesb || exit
+cd sitesb || exit
 
 # Получите последние изменения
 git pull origin main
 
 # Соберите проект
-npm install
-npm run build
+yarn
+yarn build
 
 # Перезапустите PM2
-pm2 restart your-app-name
+pm2 restart client
